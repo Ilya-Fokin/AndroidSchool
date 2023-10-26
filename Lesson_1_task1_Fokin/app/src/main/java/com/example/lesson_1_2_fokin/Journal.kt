8387a61700b7d6e318975a569541e0dd12eae601
Ilya-Fokin <fokin3349@mail.ru>
2023-10-26 20:33:39 +0400

    add changes

--- Lesson_1_task1_Fokin/app/src/main/java/com/example/lesson_1_2_fokin/Journal.kt
+++ Lesson_1_task1_Fokin/app/src/main/java/com/example/lesson_1_2_fokin/Journal.kt
@@ -2,12 +2,11 @@ package com.example.lesson_1_2_fokin
 
 import java.util.Collections
 
-data class Journal<T: MutableCollection<Student>>(val journal: T): JournalInter<T> {
-    override fun addStudent(student: Student): Boolean {
+data class Journal<T: MutableCollection<Student>>(val journal: T) {
+    fun addStudent(student: Student): Boolean {
         return journal.add(student)
     }
-
-    override fun getAllStudent(): List<Student> {
+    fun getAllStudent(): List<Student> {
         return journal.sorted()
     }
 }
